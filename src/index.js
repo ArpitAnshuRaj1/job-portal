4f25a54303475f97ff4f37b2b3d8443645435137
ArpitAnshuRaj1 <125995509+ArpitAnshuRaj1@users.noreply.github.com>
2025-06-02 16:54:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom";  // ✅ HashRouter import karein
+import { HashRouter } from "react-router-dom";  // ✅ import Hash Router here
 
 import './index.css';
 import App from './App';
@@ -8,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>   {/* ✅ Sirf yahan Router use karein */}
+    <HashRouter>   {/* use Router here */}
       <App />
     </HashRouter>
   </React.StrictMode>
